a09f9dd0f70449bebbeea2d8084ef062e283f18b
Stefan Müller zzeroo <co@zzeroo.com>
2020-04-29 21:57:29 +0200

    Fix warnings und Restrukturierte Logik

--- src/gui/gtk3/mod.rs
+++ src/gui/gtk3/mod.rs
@@ -1,15 +1,25 @@
 use gio::prelude::*;
 use gtk::prelude::*;
 
-use gtk::{Application, ApplicationWindow};
+use gtk::Application;
 
-pub fn launch(app: &gtk::Application) {
-    let glade_src = include_str!("main.ui");
-    let builder = gtk::Builder::new_from_string(glade_src);
-    let window_main: gtk::ApplicationWindow = builder
-        .get_object("window_main")
-        .expect("Couldn't get window");
-    window_main.set_application(Some(app));
+pub fn launch() {
+    let application = Application::new(
+        Some("com.gaswarnanlagen.ne4-mod-bus.ne4_konfig"),
+        Default::default(),
+    )
+    .expect("failed to initalize GTK application");
 
-    window_main.show_all();
+    application.connect_activate(|app| {
+        let glade_src = include_str!("main.ui");
+        let builder = gtk::Builder::new_from_string(glade_src);
+        let window_main: gtk::ApplicationWindow = builder
+            .get_object("window_main")
+            .expect("Couldn't get window");
+        window_main.set_application(Some(app));
+
+        window_main.show_all();
+    });
+
+    application.run(&[]);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,7 @@
-use gio::prelude::*;
-use gtk::prelude::*;
-
-use gtk::{Application, ApplicationWindow};
-
 mod gui {
     pub mod gtk3;
 }
 
 fn main() {
-    let application = Application::new(
-        Some("com.gaswarnanlagen.ne4-mod-bus.ne4_konfig"),
-        Default::default(),
-    )
-    .expect("failed to initalize GTK application");
-
-    application.connect_activate(|app| {
-        gui::gtk3::launch(app);
-    });
-
-    application.run(&[]);
+    gui::gtk3::launch();
 }
