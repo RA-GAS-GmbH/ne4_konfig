2ad86b4430918f17839d96c9035e95b4aaef4924
Stefan Müller zzeroo <co@zzeroo.com>
2020-07-30 15:04:09 +0200

    CSV and Treestore examples
    I hope I can create the treestores for Rreg and RWreg from a CSV list of that registers.

---
+++ examples/csv.rs
@@ -0,0 +1,23 @@
+use ne4_konfig::gui::gtk3::treestore_values::TreeStoreValues;
+use std::error::Error;
+use std::io;
+use std::process;
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::ReaderBuilder::new()
+        .delimiter(b';')
+        .flexible(true)
+        .from_reader(io::stdin());
+    for result in reader.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
+fn main() {
+    if let Err(err) = run() {
+        println!("error running example: {}", err);
+        process::exit(1)
+    }
+}
